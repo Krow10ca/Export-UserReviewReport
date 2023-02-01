d271c51b48fb589c82e6ac7262f809f89a234fc8
Mike Daniels <mike.daniels@simcoe.ca>
2023-02-01 13:03:12 -0500

    Update README and rename script file.

---
+++ Export-UserReviewReport.ps1
@@ -0,0 +1,129 @@
+<#
+.SYNOPSIS
+	Script overview
+.DESCRIPTION
+	Larger description of script
+.PARAMETER Param1
+    Details of parameter 1
+.PARAMETER Param2
+    Details of parameter 2
+.EXAMPLE
+	.\UserReviewReport.ps1
+	Example of running the script
+.EXAMPLE
+	.\UserReviewReport.ps1 -Param1 -Param2
+	Example of running the script with parameters
+.NOTES
+	Additional notes regarding the script
+
+	Script:		UserReviewReport.ps1
+	Author:		Mike Daniels
+    Created:		Jan-05-2023
+    Last Updated:	Jan-31-2023
+    	
+	Changelog
+		0.1		Initial version of UserReviewReport.ps1 script
+        0.2     Added basic progress bars for manager and staff processing
+        0.3     Added CSV output
+        0.4     Add some script prerequisite checks, modify output to not automatically generate the IT report.
+
+    References
+    https://social.technet.microsoft.com/Forums/ie/en-US/34cfcfaf-7783-4e9a-b93f-e660a09ae093/count-and-ad-group-membership?forum=winserverpowershell
+
+#>
+
+
+
+[CmdletBinding()]
+
+Param(
+    [switch]$HTML = $false,
+    [switch]$CSV = $false,
+    [switch]$ITReport = $false,
+    [switch]$ManagerReport = $false,
+    [string]$CSVFilename = "csvoutput.csv"
+)
+
+# Start of script
+
+# Check for script prerequisite PowerShell modules and load if available
+Try {
+    # Load the ActiveDirectory PowerShell module
+    Import-Module ActiveDirectory
+}
+Catch {
+    # Required PowerShell module ActiveDirectory is not installed, show error message and resolution details
+    Write-Host "`nError: Prerequisite PowerShell module ActiveDirectory is not installed. Please install the module in an elevated PowerShell console using:`nInstall-Module -Name ActiveDirectory`n" -ForegroundColor Red
+    Break
+}
+
+# Check for output format selections
+If ($CSV -eq $false -and $HTML -eq $false)
+{
+    # Display usage and exit script
+    Write-Host "`nError: No output format selected. Script must be run with at least one of -HTML or -CSV specified.`nUsage: .\UserReviewReport.ps1 [-HTML] [-CSV] [-ManagerReport]`n" -ForegroundColor Red
+    Break
+}
+
+
+# Retrieve list of AD users with direct reports
+$ManagementList = Get-ADUser -Filter { (directReports -like "*") -and (enabled -eq $true) } -Properties name,mail,directReports | Select-Object name,mail,directReports | Sort-Object name
+
+# Set variables for manager processing status bar
+$CurrentManager = 0
+$ManagerCount = $ManagementList.Count
+
+# Start ReportContent array
+$ReportContent = @()
+
+ForEach ($Manager in $ManagementList)
+{
+    # Start/Update managers processed progress bar
+    $CurrentManager++
+    Write-Progress -Activity "Processing Management Users" -Status "Processing Manager ($CurrentManager/$ManagerCount)" -PercentComplete (($CurrentManager / $ManagerCount) * 100) -Id 0
+    
+    $ManagerDirectReports = $Manager | Select-Object -ExpandProperty directReports
+    
+    # Set variables for direct reports processing status bar
+    $CurrentDirectReport = 0
+    $DirectReportCount = $ManagerDirectReports.Count
+    
+    ForEach ($DirectReport in $ManagerDirectReports)
+    {
+        # Increment progress bar counter and update displayed status
+        $CurrentDirectReport++
+        Write-Progress -Activity "Processing Management User's Direct Reports" -Status "Processing Employee ($CurrentDirectReport/$DirectReportCount)" -PercentComplete (($CurrentDirectReport / $DirectReportCount) * 100) -Id 1 -ParentId 0
+        
+        # Retrieve user data from Active Directory
+        $DirectReportUserData = Get-ADUser -Identity $DirectReport -properties Name,Title,SamAccountName,EmailAddress,Enabled,AccountExpirationDate,LastLogonDate,PasswordExpired,PasswordLastSet,PasswordNeverExpires
+        
+        # Add an entry for the user to the ReportContent array
+        $ReportContent += New-Object PSObject -Property @{
+            'ManagerName' = $Manager.name
+            'Name' = $DirectReportUserData.Name
+            'Title' = $DirectReportUserData.Title
+            'Email' = $DirectReportUserData.EmailAddress
+            'AccountName' = $DirectReportUserData.SamAccountName
+            'AccountEnabled' = $DirectReportUserData.Enabled
+            'AccountExpiration' = $DirectReportUserData.AccountExpirationDate
+            'AccountLastLogon' = $DirectReportUserData.LastLogonDate
+            'AccountPasswordLastSet' = $DirectReportUserData.PasswordLastSet
+            'AccountPasswordExpired' = $DirectReportUserData.PasswordExpired
+            'AccountPasswordNeverExpires' = $DirectReportUserData.PasswordNeverExpires
+        }
+    }
+
+    # Update the direct report status bar to completed
+    Write-Progress -Activity "Processing Management User's Direct Reports" -Status "Complete" -Completed -Id 1
+
+}
+
+# Update the manager status bar to completed
+Write-Progress -Activity "Processing Management Users" -Status "Complete" -Completed -Id 0
+
+If ($CSV)
+{
+    # Create CSV output file
+    $CSVOutputContent = $ReportContent | Select-Object 'ManagerName','Name','Title','Email','AccountName','AccountEnabled','AccountExpiration','AccountLastLogon','AccountPasswordLastSet','AccountPasswordExpired','AccountPasswordNeverExpires'
+    $CSVOutputContent | Export-Csv -NoTypeInformation -Path $CSVFilename
+}
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # UserReviewReport
-Initial version; learning Git and source code control
+This PowerShell script pulls a list of users from Active Directory with direct reports. It then uses the information to pull additional information regarding the direct reports and generates a CSV output file.
+
+PREREQUISITES
+PowerShell Active Directory Module
+Organizational structure implemented in Active Directory using the Manged By attribute.
+
+OPTIONS
+This script has several options that ban be set through command line execution. The defaults defined in the script are intended as a starting point.
+
+-HTML
+ Create reports in HTML. May be used in conjunction with -CSV. NOTE: This feature is not yet implemented.
+
+-CSV
+ Create reports in CSV format. May be used in conjunction with -HTML.
+
+-ReportFilename
+ Provide a filename for the report. The filename specified here will be appended with _ITReport or _ManagerReport to indicate the report type.
+
+-IncludeContacts
+ Override the default script function and include contacts when processing managers and direct reports.
+
+-ITReport
+ The -ITReport switch enables output of an IT focussed report. The report contains information about each direct report including name, job title, and email address. It also includes information around the account including the logon name, status (enabled/disabled), expiration date, last logon date, password expiry date, password last set date, and the status of the password never expires flag.
+
+-ManagerReport
+ The -ManagerReport switch enables output of a manager friendly version of the report which includes direct report name, job title, email address, and the last login time for the account.
+
+USAGE
+.\Export-UserReviewReport.ps1 [-HTML] [-CSV] [-CSVFilename <filename>] [-ITReport] [-ManagerReport]
+
+Usage example 1: Generate an IT report in CSV format using the default filename.
+.\Export-UserReviewReport.ps1 -CSV -ITReport
+
+Usage example 2: Generate IT and Manager reports in CSV format using a custom filename.
+.\Export-UserReviewReport.ps1 -CSV -ITReport -ManagerReport -ReportFilename ExampleReport
+
+KNOWN ISSUES
+2023-01-31  Handling of contact type AD accounts which specify a manager are not currently handled gracefully. Enjoy that angry red PowerShell error.
