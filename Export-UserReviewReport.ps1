14b1878ec8e70ae7ab46b16a8723fa242bab780c
Mike Daniels <mike.daniels@simcoe.ca>
2023-02-08 07:55:21 -0500

    Update main script to add new switches and options

--- Export-UserReviewReport.ps1
+++ Export-UserReviewReport.ps1
@@ -16,10 +16,10 @@
 .NOTES
 	Additional notes regarding the script
 
-	Script:		    Export-UserReviewReport.ps1
-	Author:		    Mike Daniels
-    Created:	    Jan-05-2023
-    Last Updated:	Jan-31-2023
+	Script: Export-UserReviewReport.ps1
+	Author: Mike Daniels
+    Created:    Jan-05-2023
+    Last Updated:   Jan-31-2023
     	
 	Changelog
 		0.1		Initial version of Export-UserReviewReport.ps1 script
@@ -27,6 +27,7 @@
         0.3     Added CSV output
         0.4     Add some script prerequisite checks, modify output to not automatically generate the IT report.
         0.5     Add some additional command line switches for future use, rename others for new flow.
+        0.6     Add Manager report output, sort output reports by manager name then employee name. Update default output file to include a datestamp.
 
     References
     https://social.technet.microsoft.com/Forums/ie/en-US/34cfcfaf-7783-4e9a-b93f-e660a09ae093/count-and-ad-group-membership?forum=winserverpowershell
@@ -39,9 +40,10 @@ Param(
     [switch]$CSV = $false,
     [switch]$HTML = $false,
     [switch]$IncludeContacts = $false,
+    [switch]$IncludeDisabledAccounts = $false,
     [switch]$ITReport = $false,
     [switch]$ManagerReport = $false,
-    [string]$ReportFilename = "Export-UserReviewReport"
+    [string]$ReportFilename = (Get-Date).ToString('yyyy-MM-dd') + "_Export-UserReviewReport"
 )
 
 # Start of script
@@ -72,9 +74,15 @@ If ($ITReport -eq $false -and $ManagerReport -eq $false)
     Break
 }
 
-
 # Retrieve list of AD users with direct reports
-$ManagementList = Get-ADUser -Filter { (directReports -like "*") -and (enabled -eq $true) } -Properties name,mail,directReports | Select-Object name,mail,directReports | Sort-Object name
+If ($IncludeDisabledAccounts)
+{
+    $ManagementList = Get-ADUser -Filter { (directReports -like "*") } -Properties name,mail,directReports | Select-Object name,mail,directReports | Sort-Object name
+}
+Else
+{
+    $ManagementList = Get-ADUser -Filter { (directReports -like "*") -and (enabled -eq $true) } -Properties name,mail,directReports | Select-Object name,mail,directReports | Sort-Object name
+}
 
 # Set variables for manager processing status bar
 $CurrentManager = 0
@@ -104,25 +112,46 @@ ForEach ($Manager in $ManagementList)
         # Retrieve user data from Active Directory
         $DirectReportUserData = Get-ADUser -Identity $DirectReport -properties Name,Title,SamAccountName,EmailAddress,Enabled,AccountExpirationDate,LastLogonDate,PasswordExpired,PasswordLastSet,PasswordNeverExpires
         
-        # Add an entry for the user to the ReportContent array
-        $ReportContent += New-Object PSObject -Property @{
-            'ManagerName' = $Manager.name
-            'Name' = $DirectReportUserData.Name
-            'Title' = $DirectReportUserData.Title
-            'Email' = $DirectReportUserData.EmailAddress
-            'AccountName' = $DirectReportUserData.SamAccountName
-            'AccountEnabled' = $DirectReportUserData.Enabled
-            'AccountExpiration' = $DirectReportUserData.AccountExpirationDate
-            'AccountLastLogon' = $DirectReportUserData.LastLogonDate
-            'AccountPasswordLastSet' = $DirectReportUserData.PasswordLastSet
-            'AccountPasswordExpired' = $DirectReportUserData.PasswordExpired
-            'AccountPasswordNeverExpires' = $DirectReportUserData.PasswordNeverExpires
+        # Process direct reports with enabled accounts
+        If ($DirectReportUserData.Enabled -eq $true)
+        {
+            # Add an entry for the user to the ReportContent array
+            $ReportContent += New-Object PSObject -Property @{
+                'ManagerName' = $Manager.name
+                'Name' = $DirectReportUserData.Name
+                'Title' = $DirectReportUserData.Title
+                'Email' = $DirectReportUserData.EmailAddress
+                'AccountName' = $DirectReportUserData.SamAccountName
+                'AccountEnabled' = $DirectReportUserData.Enabled
+                'AccountExpiration' = $DirectReportUserData.AccountExpirationDate
+                'AccountLastLogon' = $DirectReportUserData.LastLogonDate
+                'AccountPasswordLastSet' = $DirectReportUserData.PasswordLastSet
+                'AccountPasswordExpired' = $DirectReportUserData.PasswordExpired
+                'AccountPasswordNeverExpires' = $DirectReportUserData.PasswordNeverExpires
+            }
+        }
+
+        # Process direct reports with disabled accounts if IncludeDisabledAccounts switch is enabled
+        If ($DirectReportUserData.Enable -eq $false -and $IncludeDisabledAccounts)
+        {
+            # Add an entry for the user to the ReportContent array
+            $ReportContent += New-Object PSObject -Property @{
+                'ManagerName' = $Manager.name
+                'Name' = $DirectReportUserData.Name
+                'Title' = $DirectReportUserData.Title
+                'Email' = $DirectReportUserData.EmailAddress
+                'AccountName' = $DirectReportUserData.SamAccountName
+                'AccountEnabled' = $DirectReportUserData.Enabled
+                'AccountExpiration' = $DirectReportUserData.AccountExpirationDate
+                'AccountLastLogon' = $DirectReportUserData.LastLogonDate
+                'AccountPasswordLastSet' = $DirectReportUserData.PasswordLastSet
+                'AccountPasswordExpired' = $DirectReportUserData.PasswordExpired
+                'AccountPasswordNeverExpires' = $DirectReportUserData.PasswordNeverExpires
         }
     }
 
     # Update the direct report status bar to completed
     Write-Progress -Activity "Processing Management User's Direct Reports" -Status "Complete" -Completed -Id 1
-
 }
 
 # Update the manager status bar to completed
@@ -131,16 +160,17 @@ Write-Progress -Activity "Processing Management Users" -Status "Complete" -Compl
 # If CSV format is specified, generate CSV outputs
 If ($CSV)
 {
-    # If ITReport is selected, output the IT formmated report   
+    # If ManagerReport is selected, output the IT formatted report   
     If ($ManagerReport)
     {
-        Break
+        $ManagerReportContent =  $ReportContent | Select-Object 'ManagerName','Name','Title','Email','AccountLastLogon' | Sort-Object ManagerName,Name
+        $ManagerReportContent | Export-Csv -NoTypeInformation -Path $ReportFilename"_Manager_Format.csv"
     }
 
-    # If ITReport is selected, output the IT formmated report
+    # If ITReport is selected, output the IT formatted report
     If ($ITReport)
     {
-        $ITReportContent = $ReportContent | Select-Object 'ManagerName','Name','Title','Email','AccountName','AccountEnabled','AccountExpiration','AccountLastLogon','AccountPasswordLastSet','AccountPasswordExpired','AccountPasswordNeverExpires'
-        $ITReportContent | Export-Csv -NoTypeInformation -Path $ReportFilename
+        $ITReportContent = $ReportContent | Select-Object 'ManagerName','Name','Title','Email','AccountName','AccountEnabled','AccountExpiration','AccountLastLogon','AccountPasswordLastSet','AccountPasswordExpired','AccountPasswordNeverExpires' | Sort-Object ManagerName,Name
+        $ITReportContent | Export-Csv -NoTypeInformation -Path $ReportFilename"_IT_Format.csv"
     }
 }
